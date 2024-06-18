dc0baec24904058686d0989330c7afd2ac533690
itzy-suuport <itzygallerysu@gmail.com>
2024-06-19 01:21:34 +0900

    루트경로 앨리어싱, JSON 데이터 추가, 이미지 추가, 캐러셀 형태구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import Container from "./components/Container";
-import GridContainer from "./components/GridContainer";
+import Container from "@/components/Container";
+import GridContainer from "@/components/GridContainer";
+import supportData from "@/data/supports.json";
+// import { useState } from "react";
+
+type support = {
+  id: number;
+  cover: string;
+};
 
 const App = () => {
+  // const [selected, setSelected] = useState();
+  const supports: support[] = supportData;
+
   return (
     <Container>
       <GridContainer>
@@ -11,6 +21,44 @@ const App = () => {
 
         <div className="col-span-6 flex justify-center items-center">Test</div>
       </GridContainer>
+
+      <div className="col-span-12 relative flex p-4">
+        <div className="absolute left-0 inset-y-0 w-12 flex justify-center items-center bg-gradient-to-r from-neutral-400">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="size-6 text-white"
+          >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+          </svg>
+        </div>
+
+        <div className="flex gap-8 overflow-x-scroll snap-x scrollbar-hide">
+          {supports.map(({ cover }: support) => (
+            <img
+              src={`./images/covers/${cover}`}
+              alt=""
+              className="max-w-56 aspect-[148/210] object-cover snap-start rounded-lg shadow-xl shadow-slate-300"
+            />
+          ))}
+        </div>
+
+        <div className="absolute right-0 inset-y-0 w-12 flex justify-center items-center bg-gradient-to-l from-neutral-400">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="size-6 text-white"
+          >
+            <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+          </svg>
+        </div>
+      </div>
     </Container>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindScrollbarHide from "tailwind-scrollbar-hide";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -21,5 +23,5 @@ export default {
       ],
     },
   },
-  plugins: [],
+  plugins: [tailwindScrollbarHide],
 };
