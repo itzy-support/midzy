7d33ddbb6f01a59cdeb979c0760cb3d51bc4bb04
itzy-suuport <itzygallerysu@gmail.com>
2024-06-25 00:33:15 +0900

    PDF 다운로드 추가, 디자인 수정

--- src/pages/message-book/MessageBookDetailPage.tsx
+++ src/pages/message-book/MessageBookDetailPage.tsx
@@ -1,21 +1,27 @@
 import { MessageBook } from "@/types";
-import { getCoverURL, getPhotoURL } from "@/utils";
+import { getCoverURL, getDriveURL, getPhotoURL } from "@/utils";
+import { faBook, faFilePdf } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useLoaderData, useNavigate } from "react-router-dom";
 
 const MessageBookDetailPage = () => {
   const messageBook: MessageBook = useLoaderData() as MessageBook;
-  const { id, title, description, date, contents, path, photos } = messageBook;
+  const { id, title, description, date, contents, path, photos, pdf } = messageBook;
 
   const navigate = useNavigate();
   const goViewer = (id: number): void => {
     navigate(`/message-book/viewer/${id}`);
   };
 
+  const openPdfWindow = (pdfId: string): void => {
+    open(getDriveURL(pdfId));
+  };
+
   return (
     <main className="container mx-auto p-4 flex flex-col gap-4">
       <section className="grid grid-cols-3 gap-8 bg-white p-6 rounded-xl shadow-lg">
         {/* 이미지 */}
-        <div className="col-span-3 sm:col-span-1 flex justify-center">
+        <div className="col-span-3 lg:col-span-1 flex justify-center">
           <img
             src={getCoverURL(path)}
             alt={title}
@@ -24,11 +30,11 @@ const MessageBookDetailPage = () => {
         </div>
 
         {/* 정보 */}
-        <div className="col-span-3 sm:col-span-2 flex flex-col gap-4">
+        <div className="col-span-3 lg:col-span-2 flex flex-col gap-4">
           <div>
             <span className="text-xs opacity-55">{date}</span>
-            <h2 className="text-4xl font-semibold">{title}</h2>
-            <p className="mt-1 opacity-75">{description}</p>
+            <h2 className="text-4xl font-semibold my-1">{title}</h2>
+            <p className="opacity-75">{description}</p>
           </div>
 
           {contents && (
@@ -39,12 +45,25 @@ const MessageBookDetailPage = () => {
                 ))}
               </div>
 
-              <button
-                className="w-fit text-itzy-500 hover:text-itzy-200 transition duration-300 ease-in-out"
-                onClick={() => goViewer(id)}
-              >
-                읽어보기 📖
-              </button>
+              <div className="flex gap-4">
+                <button
+                  className="w-fit py-1 px-2.5 bg-itzy-500 text-white hover:text-itzy-200 rounded-full transition duration-300 ease-in-out"
+                  onClick={() => goViewer(id)}
+                >
+                  <FontAwesomeIcon icon={faBook} className="opacity-90" />
+                  <span className="ml-2">읽어보기</span>
+                </button>
+
+                {pdf && (
+                  <button
+                    className="w-fit hover:text-red-600 transition duration-300 ease-in-out"
+                    onClick={() => openPdfWindow(pdf)}
+                  >
+                    <FontAwesomeIcon icon={faFilePdf} className="text-red-600 opacity-90" />
+                    <span className="ml-1.5">다운로드</span>
+                  </button>
+                )}
+              </div>
             </>
           )}
         </div>
@@ -59,7 +78,7 @@ const MessageBookDetailPage = () => {
             <img
               key={index}
               src={getPhotoURL(path, index)}
-              className="rounded-md aspect-square object-cover col-span-6 sm:col-span-4 md:col-span-3 lg:col-span-2"
+              className="rounded-md aspect-square object-cover col-span-4 md:col-span-3 xl:col-span-2"
             />
           ))}
         </div>
