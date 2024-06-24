b0aad622ab8b085782be83dc4bb26e0d809ca45b
son95 <son95@sainti.co.kr>
2024-06-24 19:17:42 +0900

    FAB 구현, 애니메이션 수정 필요

--- src/pages/message-book/MessageBookViewerPage.tsx
+++ src/pages/message-book/MessageBookViewerPage.tsx
@@ -1,19 +1,32 @@
 import { MessageBook } from "@/types";
 import { getMessageBookURL } from "@/utils";
+import { faListUl } from "@fortawesome/free-solid-svg-icons/faListUl";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const MessageBookViewr = () => {
   const messageBook: MessageBook = useLoaderData() as MessageBook;
-  const { pages, path } = messageBook;
+  const { pages, path, contents } = messageBook;
 
-  // const openDriveLink = (): void => {
-  //   open(getDriveURL(pdf));
-  // };
+  const moveIndex = (index: number): void => {
+    location.href = `#${index}`;
+  };
 
-  /* <button onClick={openDriveLink}>다운</button> */
+  const contentsNumbers = document.querySelectorAll(".contents-li").length;
+  const height = `h-[${40 * contentsNumbers}px]`;
+
+  const [displayIndex, setDisplayIndex] = useState<string>("h-0");
+  const toggleDisplayIndex = () => {
+    if (displayIndex === "h-0") {
+      setDisplayIndex(height);
+    } else {
+      setDisplayIndex("h-0");
+    }
+  };
 
   return (
-    <main className="container mx-auto p-4 grid grid-cols-2 gap-1.5">
+    <main className="container mx-auto p-4 grid grid-cols-2 gap-1.5 relative">
       {[...Array(pages)].map((_, index) => (
         <img
           key={index}
@@ -22,6 +35,37 @@ const MessageBookViewr = () => {
           className="col-span-2 xl:col-span-1 aspect-[640/903] object-cover rounded-lg drop-shadow-lg"
         />
       ))}
+
+      {contents && (
+        <div className="fixed bottom-4 right-5 h-screen flex flex-col gap-2 justify-end items-end">
+          <ul
+            className={
+              "bg-white rounded-md drop-shadow-md overflow-hidden transition-[height] duration-300 ease-in-out " +
+              displayIndex
+            }
+          >
+            {contents.map(
+              (content) =>
+                !content.hidden && (
+                  <li
+                    key={content.index}
+                    className="py-2 px-4 hover:text-itzy-500 transition duration-300 ease-in-out cursor-pointer contents-li"
+                    onClick={() => moveIndex(content.index as number)}
+                  >
+                    {content.entry}
+                  </li>
+                )
+            )}
+          </ul>
+
+          <button
+            className="w-fit aspect-square p-3 bg-white rounded-full drop-shadow-md hover:text-itzy-500 transition duration-300 ease-in-out"
+            onClick={toggleDisplayIndex}
+          >
+            <FontAwesomeIcon icon={faListUl} size="lg" />
+          </button>
+        </div>
+      )}
     </main>
   );
 };
